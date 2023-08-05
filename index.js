ea3efefa7b1b18cee3cb7712d6a078c21f416094
Fadi Sarwat <fadisarwat@yahoo.com>
2023-08-06 01:56:37 +0300

    Add ip address

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ws = require('ws');
+const { networkInterfaces } = require('os');
 
 const wss = new ws.WebSocketServer({ port: 1233 });
 
@@ -19,6 +20,25 @@ let lastClientId = 1;
 
 let buzzedID = 0;
 let lockBuzz = false;
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
+console.log("IP: %s", results["en0"][0])
 
 wss.on('connection', ws => {
     ws.on('error', console.error);
